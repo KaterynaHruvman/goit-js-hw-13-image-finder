cf0f27d77672a4cb93475a0d1f7735aa0cce1b71
Ekateryna Gruvman <kate.gruvman@ukr.net>
2021-03-22 14:32:23 +0200

    button

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,45 +1,38 @@
 
-const API_KEY = '20764916-f72ac860731c159c811384133';
-const BASE_URL = 'https://pixabay.com/api/';
-const options = {
-  headers: {
-    Authorization: API_KEY,
-  },
-};
-
-export default class PicApiService {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
-
-  fetchPicteres() {
-    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.pageNumber}&per_page=${this.perPage}`;
-
-    return fetch(url, options)
-      .then(response => response.json())
-      .then(({ pictures }) => {
-        this.incrementPage();
-        return pictures;
-      });
-  }
-
-  incrementPage() {
-    this.page += 1;
-  }
-
-  resetPage() {
-    this.page = 1;
-  }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
+export default class apiService {
+    constructor() {
+        this.serchQuery = '';
+        this.page = 1;
 }
+    
+    async fetchImg() {
+   
+    const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
+    const API_KEY = '20764916-f72ac860731c159c811384133'
+    const url = `${BASE_URL}&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`
+      
+        const response = await fetch(url)
+        const newImg = response.json()
+        const markup = newImg.then(data => {
+                this.incrementPage();
+                
+                return data.hits;
+            });    
+      return markup  
+}   
+    incrementPage() {
+       this.page += 1 
+    } 
 
+    resetPage() {
+        this.page = 1;
+    }
 
-https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+     get query() {
+        return this.searchQuery
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery
+    }
+}
